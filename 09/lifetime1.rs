6f5587395599ca7ce3796253297f536a0b08792f
Ahsan Virani <ahsanvirani@Mac.fritz.box>
2025-07-10 22:08:19 +0200

    adding lifetimes

---
+++ 09/lifetime1.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+struct Borrowed<'a>(&'a i32);
+
+#[derive(Debug)]
+struct NameBorrowed<'a> {
+    x: &'a i32,
+    y: &'a i32,
+}
+
+#[derive(Debug)]
+enum Either<'a> {
+    Num(i32),
+    Ref(&'a i32),
+}
+
+fn main() {
+    let x: i32 = 18;
+    let y: i32 = 15;
+
+    let single: Borrowed = Borrowed(&x);
+    let double: NameBorrowed = NameBorrowed { x: &x, y: &y };
+    let reference: Either = Either::Ref(&x);
+    let number: Either = Either::Num(y);
+
+    println!("x is borrowed in {:?}", single);
+    println!("x and y are borrowed in {:?}", double);
+    println!("x is borrowed in {:?}", reference);
+    println!("y is *not* borrowed in {:?}", number);
+}
